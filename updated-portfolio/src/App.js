f122a8f37f1533f9e446054e7cb59d0b442f9c01
LMField82 <meneslindsay@hotmail.com>
2020-04-04 23:04:45 -0600

    initial App file

--- updated-portfolio/src/App.js
+++ updated-portfolio/src/App.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Nav from "./components/Nav";
+import Card from "./components/Card";
+import About from './pages/about';
 import './App.css';
+import Footer from './components/Footer';
 
-function App() {
+class App extends Component {
+  
+  render() {
   return (
+
    <Router>
      <div>
        <Nav />
-        {/* <Route exact path="/" component={About} />
-        {/* <Route exact path="/about" component={About} /> */}
-        {/* <Route exact path="/portfolio" component={Portfolio} />
-        <Route path="/contact" component={Contact} /> */}
+         <Route exact path="/" component={About} />
+         <Route exact path="/about" component={About} />
+        {/* <Route exact path="/portfolio" component={Portfolio} /> */}
+        {/* <Route path="/contact" component={Contact} />  */}
+        <Card />
+        <Footer />
      </div>
    </Router>
   );
+}
+
 }
 
 export default App;
