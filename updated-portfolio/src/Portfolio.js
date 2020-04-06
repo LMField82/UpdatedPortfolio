7586e22a6bd928100d9cad24d7f1ef83f4956641
LMField82 <meneslindsay@hotmail.com>
2020-04-06 15:04:23 -0600

    adding Portfolio container

---
+++ updated-portfolio/src/Portfolio.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import Nav from "./components/Nav";
+import Footer from './components/Footer';
+import './App.css';
+import About from './pages/about';
+import Projects from './pages/projects';
+import Contact from './pages/contact';
+
+class Portfolio extends Component {
+    state = {
+      currentPage: "Home"
+    };
+  
+    handlePageChange = page => {
+      this.setState({ currentPage: page });
+    };
+  
+    renderPage = () => {
+        if (this.state.currentPage === "About") {
+        return <About />;
+      } else if (this.state.currentPage === "Projects") {
+        return <Projects />;
+      } else if (this.state.currentPage === "Contact") {
+        return <Contact />;
+      } else {
+          return <About />;
+      }
+    };
+  
+    render() {
+      return (
+        <div>
+          <Nav
+            currentPage={this.state.currentPage}
+            handlePageChange={this.handlePageChange}
+          />
+          {this.renderPage()}
+          <Footer />
+        </div>
+      );
+    }
+  }
+  
+  export default Portfolio;
