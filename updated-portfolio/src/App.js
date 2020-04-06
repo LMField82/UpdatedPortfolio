ea036786d699e34bccb286b3d743819749b382ed
LMField82 <meneslindsay@hotmail.com>
2020-04-06 15:08:52 -0600

    main App file

--- updated-portfolio/src/App.js
+++ updated-portfolio/src/App.js
@@ -1,30 +1,9 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Nav from "./components/Nav";
-import Card from "./components/Card";
-import About from './pages/about';
-import './App.css';
-import Footer from './components/Footer';
+import "./Portfolio";
 
-class App extends Component {
-  
-  render() {
-  return (
 
-   <Router>
-     <div>
-       <Nav />
-         <Route exact path="/" component={About} />
-         <Route exact path="/about" component={About} />
-        {/* <Route exact path="/portfolio" component={Portfolio} /> */}
-        {/* <Route path="/contact" component={Contact} />  */}
-        <Card />
-        <Footer />
-     </div>
-   </Router>
-  );
-}
+  const App = () => <Portfolio />;
 
-}
 
 export default App;
+  
