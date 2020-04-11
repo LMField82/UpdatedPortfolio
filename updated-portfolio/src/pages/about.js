e33eb5d5b38fb5a79ccff6f1e059d1b00f25d985
LMField82 <meneslindsay@hotmail.com>
2020-04-10 19:06:09 -0600

    added card flip functionality

--- updated-portfolio/src/pages/about.js
+++ updated-portfolio/src/pages/about.js
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import "../styles/about.css";
+import ReactCardFlip from 'react-card-flip';
 
-function About() {
+const About = () => {
+    const [isFlipped, setIsFlipped] = useState(false);
+
+    const handleClick = () => {
+        setIsFlipped(!isFlipped);
+    }
+
     return (
-        <div>
-            <Nav />
-        </div>
-    );
+        <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+            <div className="card-container">
+                <div className="card front">
+                    <div className="card-body">
+                        <img src="images/Lindsay.jpeg" className="card-img-top profile" alt="Lindsay Field" />
+                        <h4 className="card-title">Hi, I'm Lindsay!</h4>
+                        <p className="card-text">I recently graduated from a full-stack coding bootcamp through the University of Denver. I learned skills in HTML, CSS, JavaScript, SQL, MongoDB, React, and more. See my full Story below.</p>
+                        <button onClick={handleClick}>About Me</button>
+                    </div>
+                </div>
+            </div>
+                  
+            <div className="card-container">
+                <div className="card-container">
+                <div className="card back">
+                    <div className="card-body">
+                        <img src="images/Lindsay.jpeg" className="card-img-top profile" alt="Lindsay Field" />
+
+                        <h4 className="card-title">Here's My Story</h4>
+                        <p className="card-text">I have a background in Mathematics and Education... </p>
+                        <a href="#" className="btn">Back</a>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </ReactCardFlip>
+    )
+
 }
+
+
 
 export default About;
